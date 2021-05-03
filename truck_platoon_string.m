9b18ae99fa8e5f820f8ca3ba913ea0c069693cc7
André de Souza Mendes <asmendes@fei.edu.br>
2021-05-03 10:22:27 -0300

    Update truck_platoon_string.m

--- truck_platoon_string.m
+++ truck_platoon_string.m
@@ -1,6 +1,6 @@
 %% Truck platooning - String stability
-% Animation of a string stable four-truck platooning at 72 km/h with 1
-% second time gap.
+% Simulation and animation of a string stable four-truck platooning at 72
+% km/h with 1 second time gap.
 %
 %%
 
